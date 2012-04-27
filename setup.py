7b3c2659251337b4daec006b91114cfc32106d53
Philip Xu <pyx@xrefactor.com>
2012-04-27 05:43:01 -0400

    added support for python 2.6

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 from distutils.core import setup
 
 VERSION = '0.3'
@@ -8,6 +9,19 @@ LONG_DESCRIPTION = open('README.rst').read()
 INSTALL_REQUIRES = [
     'beautifulsoup4',
     ]
+
+SUPPORTED_PY_VERSION = (
+        (2, 6),
+        (2, 7),
+        )
+
+PY_MAJOR, PY_MINOR = sys.version_info[:2]
+
+if (PY_MAJOR, PY_MINOR) not in SUPPORTED_PY_VERSION:
+    sys.exit('RiverCrab does not support your python version right now.')
+
+if (PY_MAJOR, PY_MINOR) == (2, 6):
+    INSTALL_REQUIRES.append('argparse')
 
 setup(
     name='rivercrab',
@@ -22,6 +36,7 @@ setup(
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Site Management',
@@ -31,7 +46,8 @@ setup(
     author='Philip Xu',
     author_email='pyx@xrefactor.com',
     url='https://bitbucket.org/pyx/rivercrab',
-    download_url='https://bitbucket.org/pyx/rivercrab/get/tip.tar.bz2',
+    download_url=(
+        'https://bitbucket.org/pyx/rivercrab/get/v%s.tar.bz2' % VERSION),
     scripts=['rivercrab'],
     license='BSD-New',
     install_requires=INSTALL_REQUIRES,
